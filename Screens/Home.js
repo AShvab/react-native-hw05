06a1beb5c12da985eb260ed5677312c74e2cae89
AShvab <shvabovska@gmail.com>
2023-08-07 02:10:46 +0300

    comments deleted

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,7 +5,7 @@ import PostsScreen from "./PostsScreen";
 import CreatePostsScreen from "./CreatePostsScreen";
 import ProfileScreen from "./ProfileScreen";
 
-import { Feather, Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { View } from "react-native";
 
 const Tabs = createBottomTabNavigator();
@@ -24,54 +24,6 @@ const ActiveIcon = ({ iconName }) => {
   );
 };
 
-// const Home = () => {
-//   return (
-//     <Tabs.Navigator
-//       screenOptions={{
-//         headerShown: false,
-//         tabBarStyle: {
-//           alignItems: "center",
-//           height: 60,
-//         },
-//         tabBarLabelStyle: { display: "none" },
-//       }}
-//     >
-//       <Tabs.Screen
-//         name="Posts"
-//         component={PostsScreen}
-//         options={{
-//           tabBarIcon: () => (
-//             <Feather name="grid" size={24} color={"rgba(33, 33, 33, 0.8)"} />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="CreatePost"
-//         component={CreatePostsScreen}
-//         options={{
-//           tabBarStyle: { display: "none" },
-//           tabBarIcon: () => (
-//             <Ionicons
-//               name="add"
-//               size={24}
-//               color={"ffffff"}
-//               style={styles.addButtonContainer}
-//             />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="Profile"
-//         component={ProfileScreen}
-//         options={{
-//           tabBarIcon: () => (
-//             <Feather name="user" size={24} color={"rgba(33, 33, 33, 0.8)"} />
-//           ),
-//         }}
-//       />
-//     </Tabs.Navigator>
-//   );
-// };
 const Home = () => {
   return (
     <Tabs.Navigator
@@ -102,11 +54,7 @@ const Home = () => {
         options={{
           tabBarStyle: { display: "none" },
           tabBarIcon: () => (
-            <Feather
-              name="plus"
-              size={24}
-              color={"rgba(33, 33, 33, 0.8)"}
-            />
+            <Feather name="plus" size={24} color={"rgba(33, 33, 33, 0.8)"} />
           ),
         }}
       />
