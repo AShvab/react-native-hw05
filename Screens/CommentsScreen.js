081f210dfcb4900198799c1d72b77b97afdee339
AShvab <shvabovska@gmail.com>
2023-08-06 22:47:16 +0300

    fixed comments-screen

--- Screens/CommentsScreen.js
+++ Screens/CommentsScreen.js
@@ -1,78 +1,235 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  TouchableWithoutFeedback,
   Image,
   Keyboard,
   Platform,
   KeyboardAvoidingView,
+  TextInput,
+  FlatList,
 } from "react-native";
 import Svg, { Path } from "react-native-svg";
 import { Feather, Ionicons } from "@expo/vector-icons";
+import posts from "../posts";
+
 import { gStyle } from "../styles/style";
 import { useNavigation } from "@react-navigation/native";
 
-const CommentsScreen = ({route}) => {
+const CommentsScreen = ({ route }) => {
   const navigation = useNavigation();
+  const [focusedInput, setFocusedInput] = useState(null);
+  const [comment, setComment] = useState("");
+
+  const handleFocus = (placeholder) => {
+    setFocusedInput(placeholder);
+  };
+
+  const handleBlur = () => {
+    setFocusedInput(null);
+  };
+
+  const clearInput = () => {
+    setComment("");
+  };
+
+  const handleSubmitComment = () => {
+    if (comment !== "") console.log("Comment submitted:", comment);
+    clearInput();
+    Keyboard.dismiss();
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={gStyle.screenContainer}>
+      <View style={gStyle.headingContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={gStyle.backButton}
+        >
+          <Svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <Path
+              d="M20 12H4"
+              stroke="#212121"
+              stroke-opacity="0.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <Path
+              d="M10 18L4 12L10 6"
+              stroke="#212121"
+              stroke-opacity="0.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </Svg>
+        </TouchableOpacity>
+        <Text style={gStyle.heading}>Коментарі</Text>
+      </View>
+      <View>
+        <Image source={route.params.img} style={styles.img} />
+      </View>
+
+      <FlatList
+        data={route.params.commentsUnderPost}
+        renderItem={({ item }) => {
+          const isCommentByUser = item.author === "user";
+
+          return (
+            <View
+              style={[
+                styles.commentContainer,
+                isCommentByUser
+                  ? styles.userCommentContainer
+                  : styles.guestCommentContainer,
+              ]}
+            >
+              <Image
+                source={
+                  isCommentByUser ? item.userCirclePhoto : item.guestCirclePhoto
+                }
+                style={[
+                  styles.commentUserImage,
+                  isCommentByUser
+                    ? styles.userCommentImage
+                    : styles.guestCommentImage,
+                ]}
+              />
+
+              <View
+                style={[
+                  styles.commentTextContainer,
+                  isCommentByUser
+                    ? styles.userCommentTextContainer
+                    : styles.guestCommentTextContainer,
+                ]}
+              >
+                <Text style={styles.commentText}>{item.text}</Text>
+                <Text style={[styles.commentDate]}>{item.date}</Text>
+              </View>
+            </View>
+          );
+        }}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
       <KeyboardAvoidingView
-        style={{}}
         behavior={Platform.OS == "ios" ? "padding" : "height"}
       >
-        <View style={gStyle.screenContainer}>
-          <View style={gStyle.headingContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              style={gStyle.backButton}
-            >
-              <Svg
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <Path
-                  d="M20 12H4"
-                  stroke="#212121"
-                  stroke-opacity="0.8"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <Path
-                  d="M10 18L4 12L10 6"
-                  stroke="#212121"
-                  stroke-opacity="0.8"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </Svg>
-            </TouchableOpacity>
-            <Text style={gStyle.heading}>Коментарі</Text>
-          </View>
-          <View>
-           <Image source={route.params.img} style={styles.img}/>
-          </View>
+        <View style={styles.bottomContainer}>
+          <TextInput
+            style={[
+              gStyle.input,
+              focusedInput === "Коментувати..." && gStyle.inputFocused,
+              styles.inputComment,
+            ]}
+            onFocus={() => handleFocus("Коментувати...")}
+            onBlur={handleBlur}
+            placeholder="Коментувати..."
+            value={comment}
+            onChangeText={setComment}
+          />
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={handleSubmitComment}
+          >
+            <Feather name="arrow-up" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+    </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   img: {
     width: "100%",
     height: 240,
-    marginTop: 40,
+    marginTop: 30,
     marginLeft: "auto",
     marginRight: "auto",
     borderRadius: 8,
+    marginBottom: 30,
+  },
+  inputComment: {
+    borderRadius: 50,
+  },
+  bottomContainer: {
+    // flex: 1,
+    position: "relative",
+    justifyContent: "flex-end",
+    marginBottom: 10,
+  },
+  submitButton: {
+    position: "absolute",
+    bottom: 24,
+    right: 8,
+    width: 34,
+    height: 34,
+    borderRadius: 24,
+    backgroundColor: "#FF6C00",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  commentContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginBottom: 20,
+  },
+  commentUserImage: {
+    width: 28,
+    height: 28,
+    borderRadius: 50,
+    marginRight: 8,
+  },
+  commentTextContainer: {
+    flex: 1,
+    backgroundColor: "#212121",
+    borderRadius: 8,
+    padding: 16,
+  },
+  commentText: {
+    fontSize: 13,
+  },
+  commentDate: {
+    fontSize: 10,
+    color: "#BDBDBD",
+    marginTop: 4,
+  },
+  userCommentContainer: {
+    flexDirection: "row-reverse",
+  },
+  guestCommentContainer: {
+    flexDirection: "row",
+  },
+  guestCommentImage: {
+    width: 28,
+    height: 28,
+    borderRadius: 50,
+    marginRight: 16,
+  },
+  userCommentTextContainer: {
+    flex: 1,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+  },
+  guestCommentTextContainer: {
+    flex: 1,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+  },
+  userCommentImage: {
+    width: 28,
+    height: 28,
+    borderRadius: 50,
+    marginLeft: 16,
   },
 });
 
--- posts.js
+++ posts.js
@@ -2,6 +2,8 @@ import Rectangle1 from "./assets/images/Rectangle1.jpg";
 import Rectangle2 from "./assets/images/Rectangle2.jpg";
 import Rectangle3 from "./assets/images/Rectangle3.jpg";
 import train from "./assets/images/train.jpg";
+import guestCircle1 from "./assets/images/guestCircle1.jpg";
+import userCircle from "./assets/images/urerCircle.jpg";
 
 export default posts = [
   {
@@ -10,7 +12,38 @@ export default posts = [
     likes: "153",
     location: "Ukraine",
     img: Rectangle1,
-    // key: "1",
+    commentsUnderPost: [
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Really love your most recent photo. I’ve been trying to capture the same thing for a few months and would love some tips!",
+        date: "09 червня, 2020 | 08:40",
+      },
+      {
+        userCirclePhoto: userCircle,
+        author: "user",
+        text: "A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.",
+        date: "09 червня, 2020 | 09:14",
+      },
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Thank you! That was very helpful!",
+        date: "09 червня, 2020 | 09:20",
+      },
+      {
+        userCirclePhoto: userCircle,
+        author: "user",
+        text: "A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.",
+        date: "09 червня, 2020 | 11:14",
+      },
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Thank you! That was very helpful!",
+        date: "09 червня, 2020 | 12:20",
+      },
+    ],
   },
   {
     name: "Захід на Чорному морі",
@@ -18,7 +51,26 @@ export default posts = [
     likes: "200",
     location: "Ukraine",
     img: Rectangle2,
-    // key: "2",
+    commentsUnderPost: [
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Really love your most recent photo. I’ve been trying to capture the same thing for a few months and would love some tips!",
+        date: "09 червня, 2020 | 08:40",
+      },
+      {
+        userCirclePhoto: userCircle,
+        author: "user",
+        text: "A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.",
+        date: "09 червня, 2020 | 09:14",
+      },
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Thank you! That was very helpful!",
+        date: "09 червня, 2020 | 09:20",
+      },
+    ],
   },
   {
     name: "8",
@@ -26,7 +78,26 @@ export default posts = [
     likes: "200",
     location: "Italy",
     img: Rectangle3,
-    // key: "3",
+    commentsUnderPost: [
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Really love your most recent photo. I’ve been trying to capture the same thing for a few months and would love some tips!",
+        date: "09 червня, 2020 | 08:40",
+      },
+      {
+        userCirclePhoto: userCircle,
+        author: "user",
+        text: "A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.",
+        date: "09 червня, 2020 | 09:14",
+      },
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Thank you! That was very helpful!",
+        date: "09 червня, 2020 | 09:20",
+      },
+    ],
   },
   {
     name: "Західний експрес",
@@ -34,6 +105,25 @@ export default posts = [
     likes: "200",
     location: "France",
     img: train,
-    // key: "4",
+    commentsUnderPost: [
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Really love your most recent photo. I’ve been trying to capture the same thing for a few months and would love some tips!",
+        date: "09 червня, 2020 | 08:40",
+      },
+      {
+        userCirclePhoto: userCircle,
+        author: "user",
+        text: "A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.",
+        date: "09 червня, 2020 | 09:14",
+      },
+      {
+        guestCirclePhoto: guestCircle1,
+        author: "guest1",
+        text: "Thank you! That was very helpful!",
+        date: "09 червня, 2020 | 09:20",
+      },
+    ],
   },
 ];
